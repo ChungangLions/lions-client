c74a8ac21958c1cef7f11db9bd6a8bdaf84385ec
Jihaeee <elinkang02@gmail.com>
2025-08-08 23:56:12 +0900

    fix: 헤더 레이아웃 수정

--- src/components/common/layout/Header.jsx
+++ src/components/common/layout/Header.jsx
@@ -6,42 +6,45 @@ import SearchBar from '../filters/SearchBar'
 
 const Header = () => {
   return (
-    <>
     <HeaderContainer>
-      <LeftBox>
-        <Logo>
-          로고
-        </Logo>
-      </LeftBox>
-      <SearchBar />
-      <RightBox>
-        <LogoutContainer>
-        <NavItem>사장님</NavItem>
-        </LogoutContainer>
-        <Link to="mypage">
-        <NavItem>마이페이지</NavItem>
-        </Link>
-      </RightBox>
+      <HeaderGroup>
+        <LeftBox>
+          <Logo>
+            로고
+          </Logo>
+        </LeftBox>
+        <SearchBar />
+        <RightBox>
+          <LogoutContainer>
+          <NavItem>사장님</NavItem>
+          </LogoutContainer>
+          <StyledLink to="mypage">
+          <NavItem>마이페이지</NavItem>
+          </StyledLink>
+        </RightBox>
+      </HeaderGroup>
+      <DividerContainer>
+        <Divider />
+      </DividerContainer>
     </HeaderContainer>
-    <div
-       style={
-        {width: '100%', 
-        height: '100%', 
-        outline: '1px black solid', 
-        outlineOffset: '-0.50px'}
-        }>
-        </div>
-      </>
   )
 }
 
 export default Header
 
+const StyledLink = styled(Link)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+`;
+
 const HeaderContainer = styled.div`
 width: 100%;
 position: relative;
 display: flex;
-flex-direction: row;
+flex-direction: column;
 align-items: center;
 justify-content: space-between;
 gap: 0px;
@@ -94,4 +97,33 @@ align-items: center;
 justify-content: center;
 padding: 10px;
 gap: 5px;
+`;
+
+const Divider = styled.div`
+width: 100%;
+position: relative;
+border-top: 1px solid #000;
+box-sizing: border-box;
+height: 1px;
+padding: 15px;
+`;
+
+const HeaderGroup = styled.div`
+width: 100%;
+position: relative;
+display: flex;
+flex-direction: row;
+align-items: center;
+justify-content: space-between;
+gap: 0px;
+text-align: left;
+font-size: 16px;
+color: #000;
+font-family: Pretendard;
+`;
+
+const DividerContainer = styled.div`
+width: 1380px;
+position: relative;
+justify-content: center;
 `;
