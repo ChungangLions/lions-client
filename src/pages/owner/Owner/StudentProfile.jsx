fccc2c95de4cee9b1ce1d4127d5bd77bb74f2fbf
Jihaeee <elinkang02@gmail.com>
2025-08-12 00:53:45 +0900

    feat: 학생단체 프로필

--- src/pages/owner/Owner/StudentProfile.jsx
+++ src/pages/owner/Owner/StudentProfile.jsx
@@ -1,9 +1,143 @@
 import React from 'react'
+import styled from 'styled-components'
+import ProfileImg from '../../../components/common/cards/ProfileImg'
+import FavoriteBtn from '../../../components/common/buttons/FavoriteBtn'
 
 const StudentProfile = () => {
   return (
-    <div>StudentProfile</div>
+    <PageContainer>
+    <ProfileSection>
+      <ProfileGroup>
+          <ImageContainer />
+          <ContentWrapper>
+            <ContentTitle>
+              중앙대학교 37대 경영학부 학생회 다움
+            </ContentTitle>
+            <DetailSection>
+              어쩌구저쩌구
+            </DetailSection>
+          </ContentWrapper>
+        
+      </ProfileGroup>
+      <ButtonGroup>
+        <FavoriteBtn/>
+      </ButtonGroup>
+    </ProfileSection>
+    <RecordSection>
+      <Title>
+        제휴 이력
+      </Title>
+      <StoreSection>
+
+      </StoreSection>
+    </RecordSection>
+    </PageContainer>
   )
 }
 
 export default StudentProfile
+
+const PageContainer = styled.div`
+gap: 92px;
+display: flex;
+flex-direction: row;
+width: 100%;
+justify-content: center;
+align-items: flex-start;
+`;
+const ProfileSection = styled.div`
+width: 363px;;
+position: relative;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: flex-start;
+gap: 40px;
+text-align: center;
+font-size: 24px;
+color: #000;
+font-family: Pretendard;
+`;
+
+const ProfileGroup = styled.div`
+align-self: stretch;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: flex-start;
+gap: 30px;
+`;
+
+const ButtonGroup = styled.div`
+display: flex;
+flex-direction: row;
+align-items: center;
+justify-content: flex-start;
+gap: 15px;
+text-align: left;
+font-size: 16px;
+`;
+
+const RecordSection = styled.div`
+width: 885px;
+position: relative;
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+justify-content: flex-start;
+gap: 10px;
+text-align: left;
+font-size: 24px;
+color: #000;
+font-family: Pretendard;
+`;
+
+const Title = styled.div`
+align-self: stretch;
+position: relative;
+font-weight: 600;
+`;
+
+const StoreSection = styled.div`
+align-self: stretch;
+display: flex;
+flex-direction: row;
+align-items: flex-start;
+justify-content: flex-start;
+flex-wrap: wrap;
+align-content: flex-start;
+gap: 25px 24px;
+font-size: 16px;
+`;
+
+const ImageContainer = styled.div`
+width: 210px;
+position: relative;
+background-color: #d9d9d9;
+height: 210px;
+border-radius: 50%;
+`;
+
+const ContentWrapper = styled.div`
+align-self: stretch;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+gap: 10px;
+`;
+
+const ContentTitle = styled.div`
+position: relative;
+font-weight: 600;
+`;
+
+const DetailSection = styled.div`
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+justify-content: flex-start;
+gap: 7px;
+text-align: left;
+font-size: 16px;
+`;
