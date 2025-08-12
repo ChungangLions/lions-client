4fb1d0ff143ae0d7b4a4e5c393668cc38a209858
Jihaeee <elinkang02@gmail.com>
2025-08-12 17:58:50 +0900

    style: 필터 버튼 추가

--- src/components/common/buttons/FilterBtn.jsx
+++ src/components/common/buttons/FilterBtn.jsx
@@ -1,24 +1,26 @@
 import React from 'react'
-import styled from 'styled-components';
+import styled from 'styled-components'
 
-const FilterBtn = ({onClick}) => {
+const FilterBtn = ({children, onClick}) => {
   return (
-    <FilterButton onClick={onClick} />
+    <FilterButton onClick={onClick}>
+        {children}
+    </FilterButton>
   )
 }
 
 export default FilterBtn
-
 
 const FilterButton = styled.button`
 position: relative;
 border-radius: 5px;
 border: 1px solid #000;
 box-sizing: border-box;
-width: 80px;;
+width: 100%;
 display: flex;
-align-items: flex-start;
-justify-content: flex-start;
+flex-direction: row;
+align-items: center;
+justify-content: center;
 padding: 10px;
 text-align: left;
 font-size: 16px;
