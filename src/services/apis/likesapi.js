d449c85870056bd264e1f4bf9b0b8ab0acda6f50
Jihaeee <elinkang02@gmail.com>
2025-08-19 23:09:00 +0900

    feat: 사장님 프로필 수정하기 api 연결중

--- src/services/apis/likesapi.js
+++ src/services/apis/likesapi.js
@@ -1,7 +1,6 @@
 
 import { getAuthAxios } from "./authAxios";
 
-const baseURL = `http://13.125.150.49:8000`;
 
 export const togglelikes = async (userId) => {
     // 처음에 사용자 인증 위해서 access 토큰 가져오기 
