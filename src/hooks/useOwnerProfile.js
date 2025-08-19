31d7441dd1497e94feb3a6c9ac6935f60979db2e
Jihaeee <elinkang02@gmail.com>
2025-08-20 02:35:59 +0900

    feat: 사장님 프로필 정보 불러오는 훅 구현

--- src/hooks/useOwnerProfile.js
+++ src/hooks/useOwnerProfile.js
@@ -3,12 +3,15 @@ import useUserStore from "../stores/userStore";
 import { getOwnerProfile } from "../services/apis/ownerAPI";
 
 const useOwnerProfile = () => {
-  const { ownerId } = useUserStore();
+ const { userId } = useUserStore();
+ const ownerId = userId
   const [ownerProfile, setOwnerProfile] = useState(null);
   const [storeName, setStoreName] = useState("");
+  const [storeType, setStoreType] = useState("");
   const [menuNames, setMenuNames] = useState([]);
   const [storeImage, setStoreImage] = useState([]);
   const [error, setError] = useState(null);
+
 
   useEffect(() => {
     const fetchOwnerData = async () => {
@@ -20,8 +23,16 @@ const useOwnerProfile = () => {
         setOwnerProfile(profile);
         setStoreName(profile.profile_name);
 
+        if (profile.business_type === 'RESTAURANT'){
+            setStoreType("일반 음식점")
+        } else if (profile.business_type === 'CAFE'){
+            setStoreType("카페 및 디저트")
+        } else if (profile.business_type === 'BAR'){
+            setStoreType("주점")
+        } else setStoreType("기타");
+
         if (Array.isArray(profile.menus)) {
-          setMenuNames(profile.menus.map(menu => menu.name));
+          setMenuNames(profile.menus.map(menu => menu.name).join(', '));
         } else {
           setMenuNames([]);
         }
@@ -31,6 +42,8 @@ const useOwnerProfile = () => {
         } else {
           setStoreImage([]);
         }
+
+        console.log("ownerProfile", profile);
       } catch (err) {
         console.error("프로필을 가져오는 데 실패했습니다:", err);
         setError(err);
@@ -40,7 +53,7 @@ const useOwnerProfile = () => {
     fetchOwnerData();
   }, [ownerId]);
 
-  return { ownerProfile, storeName, menuNames, storeImage, error };
+  return { ownerProfile, storeName, storeType, menuNames, storeImage, error };
 };
 
 export default useOwnerProfile;
