97019a6d5e542bb4140bb02207d3f8e3c517639c
Jihaeee <elinkang02@gmail.com>
2025-08-21 11:40:00 +0900

    feat: 학생단체 -> 사장님 제안서 api 추가

--- src/services/apis/proposalAPI.js
+++ src/services/apis/proposalAPI.js
@@ -18,3 +18,22 @@ export const getAIDraftProposal = async(recipient, contact_info) => {
     return response.data
     
 }
+
+export const sendAIDraftProposal = async(recipient, contact_info) => {
+    const token = localStorage.getItem("accessToken");
+    const authAxios = getAuthAxios(token); 
+
+    const requestData = {
+        recipient: recipient,
+        contact_info: contact_info
+    };
+
+    const response = await authAxios.post(`/api/proposals/ai-draft-to-student/`, JSON.stringify(requestData), {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    return response.data
+    
+}
