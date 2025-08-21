cefab8ce3f087bea8410d2fc8b362b5e4da77d2c
Jihaeee <elinkang02@gmail.com>
2025-08-21 22:02:38 +0900

    feat: ai draft api 연결

--- src/services/apis/proposalAPI.js
+++ src/services/apis/proposalAPI.js
@@ -9,7 +9,7 @@ export const getAIDraftProposal = async(recipient, contact_info) => {
         contact_info: contact_info
     };
 
-    const response = await authAxios.post(`/api/proposals/ai-draft/`, JSON.stringify(requestData), {
+    const response = await authAxios.post(`/api/proposals/ai-draft-to-student/`, JSON.stringify(requestData), {
         headers: {
             'Content-Type': 'application/json'
         }
@@ -20,7 +20,7 @@ export const getAIDraftProposal = async(recipient, contact_info) => {
 }
 
 // 제안서 생성 (전송 또는 저장 누를 시)
-export async function createProposal(createData) {
+export default async function createProposal(createData) {
   const authAxios = getAuthAxios(localStorage.getItem('accessToken'));
   const res = await authAxios.post(`/api/proposals/`, createData);
   return res.data;
