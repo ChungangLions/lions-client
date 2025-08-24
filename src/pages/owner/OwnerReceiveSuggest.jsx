fe0896f521f11b28beec919c1a80a8e58af9f5d5
Jihaeee <elinkang02@gmail.com>
2025-08-24 19:19:55 +0900

    fix: 사장님 받은 제안 사진 오류 해결

--- src/pages/owner/OwnerReceiveSuggest.jsx
+++ src/pages/owner/OwnerReceiveSuggest.jsx
@@ -221,7 +221,7 @@ const OwnerReceiveSuggest = () => {
     <PageContainer>
       <Menu />
       <ContentContainer>
-        // <SuggestSummaryBox items={summaryItems} />
+        {/* // <SuggestSummaryBox items={summaryItems} /> */}
         <SuggestSummaryBox 
           items={summaryItems} 
           onItemClick={handleStatusClick}
@@ -260,12 +260,11 @@ const CardListGrid = styled.div`
   width: 100%;
   position: relative;
   display: grid;
-  grid-template-rows: ;
   grid-template-columns: repeat(3, 447px); 
-  justify-content: start;
-  align-content: start;
+  justify-content: center;
+  align-content: center;
   column-gap: 20px;
-  row-gap: 20px;
+  row-gap: 13px;
   text-align: left;
   font-size: 18px;
   color: #000;
